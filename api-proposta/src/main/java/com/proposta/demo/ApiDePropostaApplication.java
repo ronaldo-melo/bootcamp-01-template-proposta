bc6acd3b9e4c58e6db6dd2ab8bbf42b66db674ab
Ronaldo Melo <rpm.melkor@gmail.com>
2020-10-28 15:53:36 -0300

    create project

---
+++ api-proposta/src/main/java/com/proposta/demo/ApiDePropostaApplication.java
@@ -0,0 +1,13 @@
+package com.proposta.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDePropostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDePropostaApplication.class, args);
+	}
+
+}
---
+++ api-proposta/src/test/java/com/proposta/demo/ApiDePropostaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proposta.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDePropostaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
