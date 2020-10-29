56350c11e38fd0d74a78dc00b45eef29d93fbfd7
Ronaldo Melo <rpm.melkor@gmail.com>
2020-10-29 13:23:11 -0300

    proposta entity

--- api-proposta/src/main/java/com/proposta/demo/model/Proposta.java
+++ api-proposta/src/main/java/com/proposta/demo/model/Proposta.java
@@ -1,2 +1,51 @@
-package com.proposta.demo.model;public class Proposta {
+package com.proposta.demo.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Positive;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity
+@Table(name = "proposta")
+public class Proposta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String cpfOuCnp;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String endereco;
+
+    @Positive
+    private BigDecimal salario;
+
+    public Proposta(@NotBlank String cpfOuCnp, @Email @NotBlank String email, @NotBlank String endereco, @Positive BigDecimal salario) {
+        this.cpfOuCnp = cpfOuCnp;
+        this.email = email;
+        this.endereco = endereco;
+        this.salario = salario;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Proposta proposta = (Proposta) o;
+        return Objects.equals(id, proposta.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
