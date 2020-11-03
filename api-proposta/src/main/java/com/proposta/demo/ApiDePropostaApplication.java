358e5df27ad31eb83298ce2d25e103ccc9612ba5
Ronaldo Melo <rpm.melkor@gmail.com>
2020-11-03 20:06:44 -0300

    permite ao Spring fazer o scanning de interfaces que declaram ser clientes Feign

--- api-proposta/src/main/java/com/proposta/demo/ApiDePropostaApplication.java
+++ api-proposta/src/main/java/com/proposta/demo/ApiDePropostaApplication.java
@@ -2,7 +2,10 @@ package com.proposta.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/*Diz ao Spring que a aplicação irá usar a configuração de clientes web services dinâmicas*/
+@EnableFeignClients
 @SpringBootApplication
 public class ApiDePropostaApplication {
 
