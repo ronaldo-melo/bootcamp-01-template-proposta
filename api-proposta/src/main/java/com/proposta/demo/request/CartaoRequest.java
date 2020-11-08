a70e63a3ffea27ad92f7472c40585734bc59100e
Ronaldo Melo <rpm.melkor@gmail.com>
2020-11-08 17:04:41 -0300

    Dados da request para o cartão q git status q a

--- api-proposta/src/main/java/com/proposta/demo/request/CartaoRequest.java
+++ api-proposta/src/main/java/com/proposta/demo/request/CartaoRequest.java
@@ -1,2 +1,69 @@
-package com.proposta.demo.request;public class CartaoRequest {
+package com.proposta.demo.request;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.proposta.demo.model.Cartao;
+
+import javax.validation.constraints.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class CartaoRequest {
+
+    //@UnicoValor
+    @NotBlank
+    @NotNull
+    private String id;
+
+    @NotNull
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime emitidoEm;
+
+    @NotBlank
+    @NotNull
+    private String titular;
+
+    @NotBlank
+    @NotNull
+    private Long idProposta;
+
+    public CartaoRequest() {
+
+    }
+
+    public void setId(@NotBlank String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public LocalDateTime getEmitidoEm() {
+        return emitidoEm;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public Long getIdProposta() {
+        return idProposta;
+    }
+
+    public void setEmitidoEm(@NotNull @JsonDeserialize(using = LocalDateTimeDeserializer.class) LocalDateTime emitidoEm) {
+        this.emitidoEm = emitidoEm;
+    }
+
+    public void setTitular(@NotBlank String titular) {
+        this.titular = titular;
+    }
+
+    public void setIdProposta(@NotNull @Positive Long idProposta) {
+        this.idProposta = idProposta;
+    }
+
+    public Cartao toModel(){
+        return new Cartao(UUID.fromString(this.id), this.emitidoEm, this.titular, this.idProposta);
+    }
 }
--- api-proposta/src/main/java/com/proposta/demo/response/CartaoResponse.java
+++ api-proposta/src/main/java/com/proposta/demo/response/CartaoResponse.java
@@ -1,2 +1,42 @@
-package com.proposta.demo.response;public class CartaoResponse {
+package com.proposta.demo.response;
+
+import com.proposta.demo.model.Cartao;
+
+import java.time.format.DateTimeFormatter;
+
+public class CartaoResponse {
+
+    private String titular;
+
+    private String emitidoEm;
+
+    private String id;
+
+    private Long idProposta;
+
+    public CartaoResponse(Cartao cartao) {
+        this.titular = cartao.getTitular();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+        this.emitidoEm = cartao.getEmitidoEm().format(formatter);
+        this.id = cartao.getIdCartao().toString();
+        this.idProposta = cartao.getIdProposta();
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public String getEmitidoEm() {
+        return emitidoEm;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Long getIdProposta() {
+        return idProposta;
+    }
 }
