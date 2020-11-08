f5851875c58b5e59c101dae8d7194dc64e7464b5
Ronaldo Melo <rpm.melkor@gmail.com>
2020-11-08 16:23:14 -0300

    Entidade AvisoViagem

--- api-proposta/src/main/java/com/proposta/demo/model/AvisoViagem.java
+++ api-proposta/src/main/java/com/proposta/demo/model/AvisoViagem.java
@@ -1,12 +1,16 @@
-package com.proposta.demo.model.cartao;
+package com.proposta.demo.model;
 
-import javax.persistence.Embeddable;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
-@Embeddable
+@Entity
 public class AvisoViagem {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
 
     @NotNull
     private LocalDate validoAte;
@@ -14,7 +18,11 @@ public class AvisoViagem {
     @NotBlank
     private String destino;
 
-    public AvisoViagem(@NotNull LocalDate validoAte, @NotBlank String destino) {
+    @NotNull
+    @ManyToOne
+    private Cartao cartao;
+
+    public AvisoViagem(@NotNull Cartao cartao, @NotNull LocalDate validoAte, @NotBlank String destino) {
         this.validoAte = validoAte;
         this.destino = destino;
     }
