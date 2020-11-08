6b5921667787b0206318a68396acdd341aad2f48
Ronaldo Melo <rpm.melkor@gmail.com>
2020-11-08 16:47:55 -0300

    habita schedule

--- api-proposta/src/main/java/com/proposta/demo/ApiDePropostaApplication.java
+++ api-proposta/src/main/java/com/proposta/demo/ApiDePropostaApplication.java
@@ -3,10 +3,12 @@ package com.proposta.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /*permite ao Spring fazer o scanning de interfaces que declaram ser clientes Feign*/
 @EnableFeignClients
 @SpringBootApplication
+@EnableScheduling
 public class ApiDePropostaApplication {
 
 	public static void main(String[] args) {
