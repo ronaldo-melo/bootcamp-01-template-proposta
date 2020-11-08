3550f508287cd3feeafca0e6dd014d8ee9b65c29
Ronaldo Melo <rpm.melkor@gmail.com>
2020-11-08 17:07:50 -0300

    Cartão Controller

--- api-proposta/src/main/java/com/proposta/demo/controller/CartaoController.java
+++ api-proposta/src/main/java/com/proposta/demo/controller/CartaoController.java
@@ -1,2 +1,29 @@
-package com.proposta.demo.controller;public class CartaoController {
+package com.proposta.demo.controller;
+
+import com.proposta.demo.service.RecuperaCartao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+@RestController
+@RequestMapping("/cartoes")
+public class CartaoController {
+
+    @Autowired
+    private RecuperaCartao recuperarCartao;
+
+    @GetMapping
+    public ResponseEntity<?> recuperarCartaoPorIdProposta(@RequestParam Long idProposta){
+        return recuperarCartao.getCartao(idProposta);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> recuperarCartaoPorId(@PathVariable @NotBlank String id){
+        return ResponseEntity.ok(recuperarCartao.getCartaoPorId(id));
+    }
+
 }
