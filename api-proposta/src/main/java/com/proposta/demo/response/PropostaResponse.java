61c00ac7f7b260e54a77012faa2c5fd8870b5557
Ronaldo Melo <rpm.melkor@gmail.com>
2020-11-08 17:11:50 -0300

    Proposta DTO

--- api-proposta/src/main/java/com/proposta/demo/response/PropostaResponse.java
+++ api-proposta/src/main/java/com/proposta/demo/response/PropostaResponse.java
@@ -1,2 +1,31 @@
-package com.proposta.demo.response;public class PropostaResponse {
+package com.proposta.demo.response;
+
+import com.proposta.demo.model.Proposta;
+import com.proposta.demo.model.enums.StatusAvaliacaoProposta;
+
+public class PropostaResponse {
+
+    private String nome;
+
+    private String email;
+
+    private StatusAvaliacaoProposta estadoProposta;
+
+    public PropostaResponse(Proposta proposta){
+        this.nome = proposta.getNome();
+        this.email = proposta.getEmail();
+        this.estadoProposta = proposta.getStatusAvaliacaoProposta();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public StatusAvaliacaoProposta getEstadoProposta() {
+        return estadoProposta;
+    }
 }
