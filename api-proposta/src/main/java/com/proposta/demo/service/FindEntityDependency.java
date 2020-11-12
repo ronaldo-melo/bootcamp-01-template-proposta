45bbd67ace80c3c27c9b5888f9ba02918701a02e
Ronaldo Melo <rpm.melkor@gmail.com>
2020-11-11 22:03:23 -0300

    Service FindEntityDependency

--- api-proposta/src/main/java/com/proposta/demo/service/FindEntityDependency.java
+++ api-proposta/src/main/java/com/proposta/demo/service/FindEntityDependency.java
@@ -1,2 +1,21 @@
-package com.proposta.demo.service;public class FindEntityDependency {
+package com.proposta.demo.service;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.persistence.EntityManager;
+import java.util.Optional;
+
+public class FindEntityDependency {
+                                                // CDD 1
+    public static <T> T findEntityDepedenceDyId(Class<T> entity, Object id, EntityManager manager){
+
+        T entityFinded = (T) manager.find(entity, id);
+
+        return Optional.ofNullable(entityFinded)
+                                // CDD 2
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                        "Não existe " + entity.getSimpleName() + " com o id " + id));
+    }
+    //PONTOS CDD: 2
 }
