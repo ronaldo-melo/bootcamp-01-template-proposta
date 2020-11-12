ed324dc5f01facb7d8e491b30708b99883b95f3b
Ronaldo Melo <rpm.melkor@gmail.com>
2020-11-12 10:48:25 -0300

    Filtro de dados em CartaoDTO

--- api-proposta/src/main/java/com/proposta/demo/response/CartaoResponse.java
+++ api-proposta/src/main/java/com/proposta/demo/response/CartaoResponse.java
@@ -2,18 +2,28 @@ package com.proposta.demo.response;
 
 import com.proposta.demo.model.Cartao;
 
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class CartaoResponse {
 
     private String id;
 
+    private String emitidoEm;
+
     public CartaoResponse(Cartao cartao) {
         this.id = cartao.getIdCartao().toString();
+        String timeColonPattern = "dd/MM/yyy hh:mm";
+        DateTimeFormatter timeColonFormatter = DateTimeFormatter.ofPattern(timeColonPattern);
+
+        this.emitidoEm = timeColonFormatter.format(cartao.getEmitidoEm());
     }
 
-    public String getNumeroDoCartao() {
+    public String getCartao() {
         return id;
     }
 
+    public String getEmitidoEm() {
+        return emitidoEm;
+    }
 }
