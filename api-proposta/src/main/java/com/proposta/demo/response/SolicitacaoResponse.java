e84f848c60a455f68429668f7072c066e9381229
Ronaldo Melo <rpm.melkor@gmail.com>
2020-11-18 19:00:30 -0300

    DTO - SolicitaçãoResponse

--- api-proposta/src/main/java/com/proposta/demo/response/SolicitacaoResponse.java
+++ api-proposta/src/main/java/com/proposta/demo/response/SolicitacaoResponse.java
@@ -1,2 +1,36 @@
-package com.proposta.demo.response;public class SolicitacaoRequest {
+package com.proposta.demo.response;
+
+import com.proposta.demo.model.Solicitacao;
+
+import java.time.LocalDateTime;
+
+public class SolicitacaoResponse {
+
+    private LocalDateTime instanteDaSolicitacao;
+    private String ipDoCliente;
+    private String userAgent;
+    private CartaoResponse cartao;
+
+    public SolicitacaoResponse(Solicitacao solicitacao) {
+       this.instanteDaSolicitacao = solicitacao.getInstanteDaSolicitacao();
+       this.ipDoCliente = solicitacao.getIpDoCliente();
+       this.ipDoCliente = solicitacao.getIpDoCliente();
+       this.cartao = solicitacao.getCartao().toResponse();
+    }
+
+    public LocalDateTime getInstanteDaSolicitacao() {
+        return instanteDaSolicitacao;
+    }
+
+    public String getIpDoCliente() {
+        return ipDoCliente;
+    }
+
+    public String getUserAgent() {
+        return userAgent;
+    }
+
+    public CartaoResponse getCartao() {
+        return cartao;
+    }
 }
