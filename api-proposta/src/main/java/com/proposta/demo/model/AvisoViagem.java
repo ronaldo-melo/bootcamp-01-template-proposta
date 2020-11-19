f4d66d81b5cc614109ac9a076357ef7c6e87fc63
Ronaldo Melo <rpm.melkor@gmail.com>
2020-11-18 23:27:02 -0300

    Entity - AvisoViagem

--- api-proposta/src/main/java/com/proposta/demo/model/AvisoViagem.java
+++ api-proposta/src/main/java/com/proposta/demo/model/AvisoViagem.java
@@ -1,9 +1,10 @@
 package com.proposta.demo.model;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 public class AvisoViagem {
@@ -12,19 +13,73 @@ public class AvisoViagem {
     @Id
     private Long id;
 
-    @NotNull
-    private LocalDate validoAte;
-
     @NotBlank
     private String destino;
+
+    @Future
+    private LocalDate terminoaDaViagem;
+
+    @NotNull
+    private LocalDateTime instateDoAviso;
+
+    @NotEmpty
+    private String ipDoCliente;
+
+    @NotEmpty
+    private String userAgent;
 
     @NotNull
     @ManyToOne
     private Cartao cartao;
 
-    public AvisoViagem(@NotNull Cartao cartao, @NotNull LocalDate validoAte, @NotBlank String destino) {
-        this.validoAte = validoAte;
-        this.destino = destino;
+    @Deprecated
+    public AvisoViagem(){
+
     }
 
+    public AvisoViagem(@NotBlank String destino, @Future LocalDate terminoaDaViagem,
+                       @NotEmpty String ipDoCliente, @NotEmpty String userAgent,
+                       @NotNull Cartao cartao) {
+
+        this.destino = destino;
+        this.terminoaDaViagem = terminoaDaViagem;
+        this.ipDoCliente = ipDoCliente;
+        this.userAgent = userAgent;
+        this.cartao = cartao;
+
+        this.instateDoAviso = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public LocalDate getTerminoaDaViagem() {
+        return terminoaDaViagem;
+    }
+
+    public LocalDateTime getInstateDoAviso() {
+        return instateDoAviso;
+    }
+
+    public Cartao getCartao() {
+        return cartao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AvisoViagem that = (AvisoViagem) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
