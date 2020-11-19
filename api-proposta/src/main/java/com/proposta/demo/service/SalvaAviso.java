72de1c8e31169d9160204d48ce39a56fa81dc2b1
Ronaldo Melo <rpm.melkor@gmail.com>
2020-11-18 23:23:52 -0300

    Service - SalvaAciso

---
+++ api-proposta/src/main/java/com/proposta/demo/service/SalvaAviso.java
@@ -0,0 +1,68 @@
+package com.proposta.demo.service;
+
+import com.proposta.demo.model.AvisoViagem;
+import com.proposta.demo.model.Cartao;
+import com.proposta.demo.model.enums.EstadoBloqueio;
+import com.proposta.demo.request.AvisoRequest;
+import com.proposta.demo.service.model.viagem.SolicitacaoAvisoViagem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+@Validated
+public class SalvaAviso {
+
+    @Autowired
+    private NotificaViagem notificaViagem;
+
+    @NotBlank
+    private String destino;
+
+    @Future
+    private LocalDate terminoaDaViagem;
+
+    @NotEmpty
+    private String ipDoCliente;
+
+    @NotEmpty
+    private String userAgent;
+
+    @NotNull
+    private Cartao cartao; //CDD 1
+
+    @Transactional                                                                                      //CDD 2
+    public Long salvar(UUID idCartao, HttpServletRequest httpServletRequest, AvisoRequest avisoRequest, EntityManager manager){
+
+        //CDD 3
+        SolicitacaoAvisoViagem solicitacaoAvisoViagem = new SolicitacaoAvisoViagem(avisoRequest.getTerminoaDaViagem(), avisoRequest.getDestino());
+        notificaViagem.notificar(solicitacaoAvisoViagem, idCartao);
+
+        destino = avisoRequest.getDestino();
+        terminoaDaViagem = avisoRequest.getTerminoaDaViagem();
+        userAgent = httpServletRequest.getHeader("User-Agent");
+        ipDoCliente = httpServletRequest.getRemoteAddr();
+        cartao = FindEntity.findEntityById(Cartao.class, idCartao, manager);
+        //CDD 4
+        AvisoViagem novoAvisoViagem = new AvisoViagem(destino, terminoaDaViagem, ipDoCliente, userAgent, cartao);
+
+        novoAvisoViagem = manager.merge(novoAvisoViagem);
+
+        cartao.adicinarAvisoViagem(novoAvisoViagem);
+        manager.merge(cartao);
+
+        return novoAvisoViagem.getId();
+    }
+
+}//PONTOS CDD: 5
