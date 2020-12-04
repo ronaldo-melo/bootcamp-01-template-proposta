9a86e443e76800cec2e76dc89a11ae6649118ba7
Ronaldo Melo <rpm.melkor@gmail.com>
2020-12-04 11:57:26 -0300

    Fix - teste de integração - proposta

--- api-proposta/src/test/java/com/proposta/demo/ApiDePropostaApplicationTests.java
+++ api-proposta/src/test/java/com/proposta/demo/ApiDePropostaApplicationTests.java
@@ -1,38 +1,58 @@
 package com.proposta.demo;
 
+import com.proposta.demo.controller.CartaoController;
 import com.proposta.demo.controller.PropostaController;
 import com.proposta.demo.model.Proposta;
 import com.proposta.demo.model.enums.StatusAvaliacaoProposta;
+import com.proposta.demo.request.BiometriaRequest;
+import com.proposta.demo.request.CarteiraDigitalRequest;
 import com.proposta.demo.request.PropostaRequest;
+import com.proposta.demo.service.AssociaCartaoIDcomProposta;
 import com.proposta.demo.service.AvaliaProposta;
+import com.proposta.demo.service.ServiceCartoesFactoty;
 import org.junit.Assert;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.validation.Valid;
 import java.math.BigDecimal;
+import java.util.UUID;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
+@SpringJUnitConfig(ApiDePropostaApplication.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ApiDePropostaApplicationTests {
 
-	@PersistenceContext
-	private EntityManager manager;
+	@Autowired
+	private PropostaController propostaController;
 
 	@Autowired
-	private AvaliaProposta avaliaProposta;
+	private CartaoController cartaoController;
+
+	private UUID cartaoId;
 
 	@Test
 	@Transactional
-	void deveRetornarElegivel_QuandoAvaliarProposta() {
+	@Order(1)
+	void deveRetornar201_QuandoSalvarProposta(){
 
 		//Cenário
 		String documento = "111.111.111-11";
@@ -42,14 +62,20 @@ class ApiDePropostaApplicationTests {
 		BigDecimal salario = BigDecimal.valueOf(4000.00);
 		PropostaRequest request = new PropostaRequest(documento, nome, email, endereco, salario);
 		Proposta proposta = request.toModel();
-		manager.persist(proposta);
+		UriComponentsBuilder builder = UriComponentsBuilder.newInstance();
 
 		//Ação
-		proposta.atualizaStatus(avaliaProposta.getStatusAvaliacao(proposta.obterDadosParaAnalise()));
-
+		ResponseEntity<?> response = propostaController.salvarProposta(request, builder);
 		//Avalidação
-		Assert.assertTrue(proposta.getEstadoProposta().equals(StatusAvaliacaoProposta.ELEGIVEL));
+		Assert.assertEquals(response.getStatusCode(), HttpStatus.CREATED);
 	}
 
+	@Test
+	@Order(2)
+	void deveRetornar200_QuandoConsultarProposta() throws InterruptedException {
+		Thread.sleep(7000L);
+		ResponseEntity<?> response = propostaController.getProposta(1L);
+		Assert.assertEquals(response.getStatusCode(), HttpStatus.OK);
+	}
 
 }
